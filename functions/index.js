2de57f0fa0aeb10d03b445067dbeb104a5a69c55
Andrew Stewart Gibson <andrew@goofballLogic.com>
2022-11-11 11:43:30 +0000

    secrets and stripe verfication for webook

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,36 @@
-const functions = require("firebase-functions");
+// config
+const { defineSecret } = require('firebase-functions/params');
+const stripeAPIKey = defineSecret("STRIPE_API_KEY");
+const stripeWebhookSecret = defineSecret("STRIPE_WEBHOOK_SECRET");
 
-exports.stripeWebook = functions.https.onRequest((request, response) => {
-    functions.logger.info("Stripe says", { body: request.rawBody });
-    response.send("firestripe:ok");
-});
+// deps
+const functions = require("firebase-functions");
+const { getFirestore } = require("firebase-admin/firestore");
+const { initializeApp } = require("firebase-admin/app");
+const stripe = require("stripe");
+
+// firestore
+const db = getFirestore(initializeApp());
+
+exports.stripeWebhook = functions
+    .runWith({ secrets: [stripeAPIKey, stripeWebhookSecret] })
+    .https.onRequest(async (request, response) => {
+
+        const signature = request.headers["stripe-signature"];
+        const stripeClient = stripe(stripeAPIKey.value());
+        try {
+            // verify
+            const rawBody = request.rawBody.toString();
+            const stripeEvent = await stripeClient.webhooks.constructEventAsync(rawBody, signature, stripeWebhookSecret.value());
+            // record
+            await db.collection("stripe-events").doc(stripeEvent.id).set(stripeEvent);
+            // respond
+            response.send("firestripe:ok");
+        } catch (err) {
+            // warn
+            functions.logger.warn(err);
+            // respond
+            response.status(400).send("Invalid request");
+        }
+
+    });
